b5e695a5f8c34e0c244991ee6fa3881c1a93d154
yolgan67 <yunusolgan@gmail.com>
2022-01-31 14:19:15 +0300

    I  have created a new file,coderbyte and I have added  first solution

---
+++ CoderByte Solutions/Easy Questions/LetterUpperCase.js
@@ -0,0 +1,14 @@
+//LetterUpperCase
+// You have to UpperCase every first letter
+// for Ex "hello everybody" => "Hello Everybody"
+
+function letterUpperCase(str){
+  let arr=[]
+  newStr = str.split(" ")
+  newStr.map(item=>console.log(arr.push(item[0].toUpperCase()+item.slice(1,item.length).toLowerCase())))
+  console.log(newStr)
+  return arr.join(" ")
+  
+  }
+  let text="merhaba yENİ DünYa merHABA SAna"
+  console.log(letterUpperCase(text))
